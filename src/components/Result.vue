f150b21ac69e6c73b7aef9b7b0c3960930b1c47d
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2023-10-29 14:08:55 +0400

    first commit

---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">Ranking: {{ rankName }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rankName: null,
+    };
+  },
+  mounted() {
+    if (this.score <= 250) {
+      this.rankName = "NINJA";
+    } else if (this.score <= 350) {
+      this.rankName = "HALF-NINJA";
+    } else {
+      this.rankName = "TURTLE";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
